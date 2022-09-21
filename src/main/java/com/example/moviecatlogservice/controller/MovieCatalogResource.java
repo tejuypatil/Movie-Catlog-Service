7ed2b64130d46ea6ee8c253d1d9286e259a222bc
tejuypatil <tejuypatil@gmail.com>
2022-09-22 01:37:54 +0530

    Added microservice of movie catalog

---
+++ src/main/java/com/example/moviecatlogservice/controller/MovieCatalogResource.java
@@ -0,0 +1,43 @@
+package com.example.moviecatlogservice.controller;
+
+import com.example.moviecatlogservice.module.CatalogItem;
+import com.example.moviecatlogservice.module.Movie;
+import com.example.moviecatlogservice.module.Rating;
+import com.example.moviecatlogservice.module.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getcatalog(@PathVariable("userId") String userId){
+
+       UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating ->{
+            //for each movie ID, call movie info service and get details
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class);
+            //Put them all together
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+        }).collect(Collectors.toList());
+    }
+}
+ /* Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class)
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            */
