16d4418bc3d7e60c0dac29050211b4f1146571e8
tejuypatil <tejuypatil@gmail.com>
2022-09-22 17:05:05 +0530

    Added changes of microservice of movie catalog

--- src/main/java/com/example/moviecatlogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/example/moviecatlogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.moviecatlogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,7 +10,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
     @Bean
-    public RestTemplate getRestTemplate(){
+    @LoadBalanced
+    public RestTemplate getRestTemplate()
+    {
         return new RestTemplate();
     }
     @Bean
--- src/main/java/com/example/moviecatlogservice/controller/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatlogservice/controller/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import com.example.moviecatlogservice.module.Movie;
 import com.example.moviecatlogservice.module.Rating;
 import com.example.moviecatlogservice.module.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,16 +22,20 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Autowired
     private WebClient.Builder webClientBuilder;
     @RequestMapping("/{userId}")
     public List<CatalogItem> getcatalog(@PathVariable("userId") String userId){
 
-       UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+       UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating ->{
             //for each movie ID, call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movie/" + rating.getMovieId(), Movie.class);
             //Put them all together
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         }).collect(Collectors.toList());
